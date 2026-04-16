01853370ac392c7934f02a707d81e02ce4750897
Leo Haker <haker338@gmail.com>
2026-04-16 20:43:02 +0200

    KLAR

--- 1/src/Chatterbot.hs
+++ 1/src/Chatterbot.hs
@@ -1,3 +1,5 @@
+-- Leo Haker, Kalle Skog --
+
 {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 module Chatterbot where
 import Utilities
@@ -29,7 +31,6 @@ newtype Rule = Rule (Pattern String, [Template String])
   deriving (Eq, Show)
 
 type BotBrain = [Rule]
-
 
 
 chatterbot :: String -> [(String, [String])] -> IO ()
@@ -65,29 +66,11 @@ makePair (Rule rule) = do
         template = pick random templates
     return (pattern, template)
 
--- funkarDetta1 = (mkPattern "*" (Prelude.words"My name is *"), mkPattern "*" (Prelude.words "Je m'appelle *"))
--- funkarDetta2 = (mkPattern "+" (Prelude.words"My name a *"), mkPattern "+" (Prelude.words "Je m'buuu *"))
--- medDetta1 = ["My", "name", "is", "Zacharias"]
-
--- funkis =[funkarDetta2, funkarDetta1]
-
--- >>>  rulesApply funkis medDetta1
--- ["Je","m'appelle","Zacharias"]
 
 rulesApply :: [(Pattern String, Template String)] -> Phrase -> Phrase
 {- TO BE WRITTEN -}
 rulesApply pts phrase = fromMaybe [] (transformationsApply reflect pts phrase)
 
-
-
--- >>>reflect ["i", "will", "never", "see", "my","reflection", "in", "your", "eyes"] 
-
-{-
-Facit 
-["you", "will", "never", "see", "your",
-         "reflection", "in", "my", "eyes"]
--}
-        
 reflect :: Phrase -> Phrase
 {- TO BE WRITTEN -} 
 reflect ps = map(\p -> foldr(\r acc -> if p == (fst r) then (snd r) else acc)p reflections) ps
@@ -127,16 +110,7 @@ rulesCompile = map ruleCompile
 
 ruleCompile :: (String, [String]) -> Rule
 {- TO BE WRITTEN -}
-ruleCompile (p, ts) = Rule(starPattern $ map toLower p, map starPattern ts) -- samma som map(\t starPattern $ map toLower t)ts)
-
---------------------------------------
-
-
--- We can make a pattern from a list of elements
--- If we choose one element that represents the wildcard
--- >>> mkPattern '*' "Hi *!"
--- Pattern [Item 'H',Item 'i',Item ' ',Wildcard,Item '!']
-
+ruleCompile (p, ts) = Rule(starPattern $ map toLower p, map starPattern ts) 
 
 
 mkPattern :: Eq a => a -> [a] -> Pattern a
@@ -167,14 +141,6 @@ reductions = (map . map2) (starPattern, starPattern)
 reduce :: Phrase -> Phrase
 reduce = reductionsApply reductions
 
--- TEST -- 
-
--- >>> prepare "can you please tell me what Haskell is" 
--- ["what","is","haskell"]
-
--- Facit --
--- Bör vara: what is Haskell?
-
 reductionsApply :: [(Pattern String, Pattern String)] -> Phrase -> Phrase
 {- TO BE WRITTEN -}
 reductionsApply pts = fix f
@@ -198,82 +164,20 @@ substitute (Pattern t) s =  concatMap(\x -> case x of
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
 
-
-
-
--- >>>match (mkPattern '*' "frodo") "gandalf" 
--- >>>match (mkPattern 'x' "2*x+3+x") "2*7+3" 
--- >>>match (mkPattern 'x' "abcd") "abcd" 
--- >>>match (mkPattern 2 [1,3..5]) [1,3..5]
--- >>>match (mkPattern 'x' "2*x+3") "2*7+3"
--- >>>match (mkPattern '*' "* and *") "you and me"
--- Nothing
--- Nothing
--- Just ""
--- Just []
--- Just "7"
--- Just "you"
-
-
-{-
-FACIT:
-Nothing
-Nothing
-Just[]
-Just[]
-Just["7"]
-Just["you"]
-
--}
-p1 :: Pattern Char
-p1 = mkPattern '*' "hej"
-p2 :: Pattern Char
-p2 = mkPattern '*' "h*ej"
-
-s1 :: [Char] 
-s1 = "hej"
-s2 :: [Char] 
-s2 = "bheej"
--- >>> match p1 s1
--- >>> match p2 s2
--- Just ""
--- Nothing
-a = mkPattern '*' "* and *"
--- >>>match a "you and me"
--- Just "you"
 match :: Eq a => Pattern a -> [a] -> Maybe [a]
-match (Pattern []) [] = Just []  -- Vi saknade detta base case
+match (Pattern []) [] = Just []  
 match (Pattern _) [] = Nothing
 match (Pattern []) _ = Nothing
 match pattern@(Pattern (p:ps)) list@(x:xs) 
       | p == Wildcard = orElse (singleWildcardMatch pattern list) (longerWildcardMatch pattern list)
-      | p == Item x = match (Pattern ps) xs -- kör match efter tagit bort nästa element
-      | otherwise = Nothing -- de två tidigare, om listorna inte är samma
+      | p == Item x = match (Pattern ps) xs 
+      | otherwise = Nothing 
 
 -- Helper function to match
 singleWildcardMatch, longerWildcardMatch :: Eq a => Pattern a -> [a] -> Maybe [a]
 singleWildcardMatch (Pattern (Wildcard:ps)) (x:xs) = mmap (const [x]) (match (Pattern ps) xs)
 longerWildcardMatch _ [] = Nothing
 longerWildcardMatch pattern@(Pattern (Wildcard:ps)) (x:xs) = mmap (x:) (match pattern xs)
-
-{- TO BE WRITTEN -}
-{- OVAN ÄR EKVIVALENT MED TIDIGARE SKRIVNA NEDAN:
-
-singleWildcardMatch (Pattern (Wildcard:ps)) (x:xs) =
-  case match (Pattern ps) xs of
-    Nothing -> Nothing
-    Just _ -> Just [x]
-
-longerWildcardMatch (Pattern (Wildcard:ps)) xs = 
-    case xs of 
-        [] -> Nothing 
-        (x:xs') -> case match (Pattern (Wildcard:ps)) xs' of
-                    Nothing -> Nothing
-                    Just list -> Just(x:list)
--}
-
-
-
 
 
 -------------------------------------------------------
@@ -282,7 +186,7 @@ longerWildcardMatch (Pattern (Wildcard:ps)) xs =
 
 -- Helper function: Matches a pattern and applies the transformation
 matchAndTransform :: Eq a => ([a] -> [a]) -> Pattern a -> [a] -> Maybe [a]
-matchAndTransform transform pat = (mmap transform) . (match pat) -- f . g = f (g x). Alltså betyder detta mmap tranform (match pat xs)
+matchAndTransform transform pat = (mmap transform) . (match pat) 
 
 -- Applying a single pattern
 transformationApply :: Eq a => ([a] -> [a]) -> [a] -> (Pattern a, Template a) -> Maybe [a]
@@ -298,43 +202,3 @@ transformationsApply transform (pt: pts) xs = orElse success tryAgain
   where
     success = transformationApply transform xs pt
     tryAgain = transformationsApply transform pts xs
-
-
--- TESTER --
-{-}
-frenchPresentation :: (Pattern Char, Template Char)
-frenchPresentation = (mkPattern '*' "My name is *", mkPattern '*' "Je m'appelle *")
-frenchPresentation1 = (mkPattern '+' "My name is *", mkPattern '+' "Kalle anka *")
-frenchPresentation2 = (mkPattern '*' "My name is +", mkPattern '*' "fel fel +")
-frenchPresentation3 = (mkPattern '+' "My name is +", mkPattern '+' "Jag heter +")
-
-funkarDetta = (mkPattern "*" (Prelude.words"My name is *"), mkPattern "*" (Prelude.words "Je m'appelle *"))
-ab =  (mkPattern '*' "My name is *", mkPattern '*' "Je m'appelle *")
-medDetta = ["My", "name", "is", "Zacharias"]
-
-test1 = [frenchPresentation1, frenchPresentation2]
-
-test2 :: [(Pattern Char, Pattern Char)]
-test2 = [frenchPresentation1, frenchPresentation2, frenchPresentation]
-test3 = [frenchPresentation1, frenchPresentation2, frenchPresentation3]
-
--- >>> transformationApply id "My name is Zacharias" frenchPresentation
--- Just "Je m'appelle Zacharias"
--- >>>  Prelude.unwords $ Data.fromMaybe [] (transformationApply id medDetta funkarDetta)
--- "Je m'appelle Zacharias"
-
--- >>> ab
--- >>> funkarDetta
--- (Pattern [Item 'M',Item 'y',Item ' ',Item 'n',Item 'a',Item 'm',Item 'e',Item ' ',Item 'i',Item 's',Item ' ',Wildcard],Pattern [Item 'J',Item 'e',Item ' ',Item 'm',Item '\'',Item 'a',Item 'p',Item 'p',Item 'e',Item 'l',Item 'l',Item 'e',Item ' ',Wildcard])
--- (Pattern [Item "My name is *"],Pattern [Item "Je m'appelle *"])
-
-
-
--- >>> transformationsApply id test1 "My name is Zacharias"
--- >>> transformationsApply id test2 "My name is Zacharias"
--- >>> transformationsApply id test3 "My name is Zacharias"
--- Nothing
--- Just "Je m'appelle Zacharias"
--- Just "Jag heter Zacharias"
-
--}
