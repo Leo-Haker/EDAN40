145bfc1b444b473cda7913433b32174c62dac7d0
KalleSkog <kalle.skog@hotmail.com>
2026-05-05 14:48:39 +0200

    fixade execute tror jag

--- 3/src/Statement.hs
+++ 3/src/Statement.hs
@@ -28,7 +28,7 @@ begin = accept "begin" -# many parse #- require "end" >-> Begin
 
 while = accept "while" -# Expr.parse #- require "do" # parse >-> uncurry While
 
-read = accept "read" -# word #- require ";" >-> Read
+read' = accept "read" -# word #- require ";" >-> Read
 
 write = accept "write" -# Expr.parse #- require ";" >-> Write
 
@@ -45,8 +45,16 @@ class Executable t where
 
 instance Executable Statement where
     -- execute :: [Statement] -> Dictionary.T String Integer -> [Integer] -> [Integer]
+    execute [] dict input = []
+    execute (Assignment str expr: stmts) dict input = 
+        case Expr.value expr dict of
+            Left err -> error err
+            Right v -> 
+                execute stmts (Dictionary.insert str v dict) input
+            -- slänger in värdet på str platsen o sen kör vi vidare
+
     execute (If cond thenStmts elseStmts: stmts) dict input =
-        case (Expr.value cond dict) of
+        case Expr.value cond dict of
             Left err -> error err
             Right v ->
                 if v > 0 then
@@ -54,7 +62,33 @@ instance Executable Statement where
                 else
                     execute (elseStmts: stmts) dict input
 
+    -- stmt är in lista så vi kör ++
+    execute (Begin stmt:stmts) dict input = execute (stmt ++ stmts) dict input
+
+    execute (While cond thenstmt:stmts) dict input = 
+        case Expr.value cond dict of
+            Left err -> error err
+            Right v -> 
+                if v > 0 then
+                    -- här kör vi först then blocket sen kör vi hela while igen + resten
+                    execute (thenstmt : While cond thenstmt : stmts) dict input
+                else
+                    execute stmts dict input
+            
+    execute (Read str : stmts) dict input =
+        case input of 
+            -- eller kasta ett error med no input ex
+            [] -> execute stmts dict input
+            x:xs -> 
+                execute stmts (Dictionary.insert str x dict) xs
+    execute (Write expr : stmts) dict input = 
+        case Expr.value expr dict of
+            Left err -> error err
+            Right v ->
+                v : execute stmts dict input
+    execute (Skip : stmts) dict input = execute stmts dict input
+
 instance Parse Statement where
 -- mucho importante att assignment är till höger då keywords spelar roll i ordningen pga require
-  parse = begin ! if' ! while ! read ! write ! skip ! assignment 
+  parse = begin ! if' ! while ! read' ! write ! skip ! assignment 
   toString = error "Statement.toString not implemented"
